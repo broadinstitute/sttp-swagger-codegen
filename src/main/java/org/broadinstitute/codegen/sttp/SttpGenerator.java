d84fff2aae4566a3abf1191fd15f2484007e4f62
Oliver A Ruebenacker <oliverr@broadinstitute.org>
2019-10-01 18:27:39 -0400

    Ripping off scala module as a starting point

--- src/main/java/org/broadinstitute/codegen/sttp/SttpGenerator.java
+++ src/main/java/org/broadinstitute/codegen/sttp/SttpGenerator.java
@@ -1,191 +1,234 @@
-package org.broadinstitute.codegen.sttp;
+package io.swagger.codegen.languages;
 
 import io.swagger.codegen.*;
-import io.swagger.models.properties.*;
 
-import java.util.*;
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
 
-public class SttpGenerator extends DefaultCodegen implements CodegenConfig {
+import org.apache.commons.lang3.StringUtils;
 
-  // source folder where to write the files
-  protected String sourceFolder = "src";
-  protected String apiVersion = "1.0.0";
+public class SttpGenerator extends AbstractScalaCodegen implements CodegenConfig {
+  protected String authScheme = "";
+  protected String gradleWrapperPackage = "gradle.wrapper";
+  protected boolean authPreemptive;
+  protected boolean asyncHttpClient = !authScheme.isEmpty();
+  protected String groupId = "io.swagger";
+  protected String artifactId = "swagger-scala-client";
+  protected String artifactVersion = "1.0.0";
+  protected String clientName = "AsyncClient";
 
-  /**
-   * Configures the type of generator.
-   * 
-   * @return  the CodegenType for this generator
-   * @see     io.swagger.codegen.CodegenType
-   */
+  public SttpGenerator() {
+    super();
+    outputFolder = "generated-code/scala";
+    modelTemplateFiles.put("model.mustache", ".scala");
+    apiTemplateFiles.put("api.mustache", ".scala");
+    embeddedTemplateDir = templateDir = "scala";
+    apiPackage = "io.swagger.client.api";
+    modelPackage = "io.swagger.client.model";
+
+    setReservedWordsLowerCase(
+            Arrays.asList(
+                    // local variable names used in API methods (endpoints)
+                    "path", "contentTypes", "contentType", "queryParams", "headerParams",
+                    "formParams", "postBody", "mp", "basePath", "apiInvoker",
+
+                    // scala reserved words
+                    "abstract", "case", "catch", "class", "def", "do", "else", "extends",
+                    "false", "final", "finally", "for", "forSome", "if", "implicit",
+                    "import", "lazy", "match", "new", "null", "object", "override", "package",
+                    "private", "protected", "return", "sealed", "super", "this", "throw",
+                    "trait", "try", "true", "type", "val", "var", "while", "with", "yield")
+    );
+
+    additionalProperties.put(CodegenConstants.INVOKER_PACKAGE, invokerPackage);
+    additionalProperties.put(CodegenConstants.GROUP_ID, groupId);
+    additionalProperties.put(CodegenConstants.ARTIFACT_ID, artifactId);
+    additionalProperties.put(CodegenConstants.ARTIFACT_VERSION, artifactVersion);
+    additionalProperties.put("asyncHttpClient", asyncHttpClient);
+    additionalProperties.put("authScheme", authScheme);
+    additionalProperties.put("authPreemptive", authPreemptive);
+    additionalProperties.put("clientName", clientName);
+    additionalProperties.put(CodegenConstants.STRIP_PACKAGE_NAME, stripPackageName);
+
+    supportingFiles.add(new SupportingFile("pom.mustache", "", "pom.xml"));
+    supportingFiles.add(new SupportingFile("apiInvoker.mustache",
+            (sourceFolder + File.separator + invokerPackage).replace(".", java.io.File.separator), "ApiInvoker.scala"));
+    supportingFiles.add(new SupportingFile("client.mustache",
+            (sourceFolder + File.separator + invokerPackage).replace(".", java.io.File.separator), clientName + ".scala"));
+    supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
+    supportingFiles.add(new SupportingFile("gitignore.mustache", "", ".gitignore"));
+    // gradle settings
+    supportingFiles.add(new SupportingFile("build.gradle.mustache", "", "build.gradle"));
+    supportingFiles.add(new SupportingFile("settings.gradle.mustache", "", "settings.gradle"));
+    supportingFiles.add(new SupportingFile("gradle.properties.mustache", "", "gradle.properties"));
+    // gradleWrapper files
+    supportingFiles.add(new SupportingFile( "gradlew.mustache", "", "gradlew") );
+    supportingFiles.add(new SupportingFile( "gradlew.bat.mustache", "", "gradlew.bat") );
+    supportingFiles.add(new SupportingFile( "gradle-wrapper.properties.mustache",
+            gradleWrapperPackage.replace( ".", File.separator ), "gradle-wrapper.properties") );
+    supportingFiles.add(new SupportingFile( "gradle-wrapper.jar",
+            gradleWrapperPackage.replace( ".", File.separator ), "gradle-wrapper.jar") );
+
+    supportingFiles.add(new SupportingFile("build.sbt.mustache", "", "build.sbt"));
+
+    importMapping.remove("List");
+    importMapping.remove("Set");
+    importMapping.remove("Map");
+
+    importMapping.put("Date", "java.util.Date");
+    importMapping.put("ListBuffer", "scala.collection.mutable.ListBuffer");
+
+    typeMapping = new HashMap<String, String>();
+    typeMapping.put("enum", "NSString");
+    typeMapping.put("array", "List");
+    typeMapping.put("set", "Set");
+    typeMapping.put("boolean", "Boolean");
+    typeMapping.put("string", "String");
+    typeMapping.put("int", "Int");
+    typeMapping.put("long", "Long");
+    typeMapping.put("float", "Float");
+    typeMapping.put("byte", "Byte");
+    typeMapping.put("short", "Short");
+    typeMapping.put("char", "Char");
+    typeMapping.put("double", "Double");
+    typeMapping.put("object", "Any");
+    typeMapping.put("file", "File");
+    typeMapping.put("binary", "Array[Byte]");
+    typeMapping.put("ByteArray", "Array[Byte]");
+    typeMapping.put("ArrayByte", "Array[Byte]");
+    typeMapping.put("date-time", "Date");
+    typeMapping.put("DateTime", "Date");
+
+    instantiationTypes.put("array", "ListBuffer");
+    instantiationTypes.put("map", "HashMap");
+
+    cliOptions.add(new CliOption(CodegenConstants.MODEL_PROPERTY_NAMING, CodegenConstants.MODEL_PROPERTY_NAMING_DESC).defaultValue("camelCase"));
+  }
+
+  @Override
+  public void processOpts() {
+    super.processOpts();
+    if (additionalProperties.containsKey(CodegenConstants.MODEL_PROPERTY_NAMING)) {
+      setModelPropertyNaming((String) additionalProperties.get(CodegenConstants.MODEL_PROPERTY_NAMING));
+    }
+  }
+
+  public void setModelPropertyNaming(String naming) {
+    if ("original".equals(naming) || "camelCase".equals(naming) ||
+            "PascalCase".equals(naming) || "snake_case".equals(naming)) {
+      this.modelPropertyNaming = naming;
+    } else {
+      throw new IllegalArgumentException("Invalid model property naming '" +
+              naming + "'. Must be 'original', 'camelCase', " +
+              "'PascalCase' or 'snake_case'");
+    }
+  }
+
+  public String getModelPropertyNaming() {
+    return this.modelPropertyNaming;
+  }
+  @Override
+  public String toVarName(String name) {
+    // sanitize name
+    name = sanitizeName(name); // FIXME: a parameter should not be assigned. Also declare the methods parameters as 'final'.
+
+    if("_".equals(name)) {
+      name = "_u";
+    }
+
+    // if it's all uppper case, do nothing
+    if (name.matches("^[A-Z_]*$")) {
+      return name;
+    }
+
+    name = getNameUsingModelPropertyNaming(name);
+
+    // for reserved word or word starting with number, append _
+    if (isReservedWord(name) || name.matches("^\\d.*")) {
+      name = escapeReservedWord(name);
+    }
+
+    return name;
+  }
+
+  @Override
+  public String toParamName(String name) {
+    // should be the same as variable name
+    return toVarName(name);
+  }
+
+  public String getNameUsingModelPropertyNaming(String name) {
+    switch (CodegenConstants.MODEL_PROPERTY_NAMING_TYPE.valueOf(getModelPropertyNaming())) {
+      case original:    return name;
+      case camelCase:   return camelize(name, true);
+      case PascalCase:  return camelize(name);
+      case snake_case:  return underscore(name);
+      default:          throw new IllegalArgumentException("Invalid model property naming '" +
+              name + "'. Must be 'original', 'camelCase', " +
+              "'PascalCase' or 'snake_case'");
+    }
+
+  }
+
+  @Override
   public CodegenType getTag() {
     return CodegenType.CLIENT;
   }
 
-  /**
-   * Configures a friendly name for the generator.  This will be used by the generator
-   * to select the library with the -l flag.
-   * 
-   * @return the friendly name for the generator
-   */
+  @Override
   public String getName() {
-    return "sttp";
+    return "scala";
   }
 
-  /**
-   * Returns human-friendly help for the generator.  Provide the consumer with help
-   * tips, parameters here
-   * 
-   * @return A string value for the help message
-   */
+  @Override
   public String getHelp() {
-    return "Generates a sttp client library.";
+    return "Generates a Scala client library (beta).";
   }
 
-  public SttpGenerator() {
-    super();
-
-    // set the output folder here
-    outputFolder = "generated-code/sttp";
-
-    /**
-     * Models.  You can write model files using the modelTemplateFiles map.
-     * if you want to create one template for file, you can do so here.
-     * for multiple files for model, just put another entry in the `modelTemplateFiles` with
-     * a different extension
-     */
-    modelTemplateFiles.put(
-      "model.mustache", // the template to use
-      ".sample");       // the extension for each file to write
-
-    /**
-     * Api classes.  You can write classes for each Api file with the apiTemplateFiles map.
-     * as with models, add multiple entries with different extensions for multiple files per
-     * class
-     */
-    apiTemplateFiles.put(
-      "api.mustache",   // the template to use
-      ".sample");       // the extension for each file to write
-
-    /**
-     * Template Location.  This is the location which templates will be read from.  The generator
-     * will use the resource stream to attempt to read the templates.
-     */
-    templateDir = "sttp";
-
-    /**
-     * Api Package.  Optional, if needed, this can be used in templates
-     */
-    apiPackage = "io.swagger.client.api";
-
-    /**
-     * Model Package.  Optional, if needed, this can be used in templates
-     */
-    modelPackage = "io.swagger.client.model";
-
-    /**
-     * Reserved words.  Override this with reserved words specific to your language
-     */
-    reservedWords = new HashSet<String> (
-      Arrays.asList(
-        "sample1",  // replace with static values
-        "sample2")
-    );
-
-    /**
-     * Additional Properties.  These values can be passed to the templates and
-     * are available in models, apis, and supporting files
-     */
-    additionalProperties.put("apiVersion", apiVersion);
-
-    /**
-     * Supporting Files.  You can write single files for the generator with the
-     * entire object tree available.  If the input file has a suffix of `.mustache
-     * it will be processed by the template engine.  Otherwise, it will be copied
-     */
-    supportingFiles.add(new SupportingFile("myFile.mustache",   // the input template or file
-      "",                                                       // the destination folder, relative `outputFolder`
-      "myFile.sample")                                          // the output file
-    );
-
-    /**
-     * Language Specific Primitives.  These types will not trigger imports by
-     * the client generator
-     */
-    languageSpecificPrimitives = new HashSet<String>(
-      Arrays.asList(
-        "Type1",      // replace these with your types
-        "Type2")
-    );
-  }
-
-  /**
-   * Escapes a reserved word as defined in the `reservedWords` array. Handle escaping
-   * those terms here.  This logic is only called if a variable matches the reserved words
-   * 
-   * @return the escaped term
-   */
   @Override
-  public String escapeReservedWord(String name) {
-    return "_" + name;  // add an underscore to the name
-  }
-
-  /**
-   * Location to write model files.  You can use the modelPackage() as defined when the class is
-   * instantiated
-   */
-  public String modelFileFolder() {
-    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
-  }
-
-  /**
-   * Location to write api files.  You can use the apiPackage() as defined when the class is
-   * instantiated
-   */
-  @Override
-  public String apiFileFolder() {
-    return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
-  }
-
-  /**
-   * Optional - type declaration.  This is a String which is used by the templates to instantiate your
-   * types.  There is typically special handling for different property types
-   *
-   * @return a string value used as the `dataType` field for model templates, `returnType` for api templates
-   */
-  @Override
-  public String getTypeDeclaration(Property p) {
-    if(p instanceof ArrayProperty) {
-      ArrayProperty ap = (ArrayProperty) p;
-      Property inner = ap.getItems();
-      return getSwaggerType(p) + "[" + getTypeDeclaration(inner) + "]";
+  public String toOperationId(String operationId) {
+    // throw exception if method name is empty
+    if (StringUtils.isEmpty(operationId)) {
+      throw new RuntimeException("Empty method name (operationId) not allowed");
     }
-    else if (p instanceof MapProperty) {
-      MapProperty mp = (MapProperty) p;
-      Property inner = mp.getAdditionalProperties();
-      return getSwaggerType(p) + "[String, " + getTypeDeclaration(inner) + "]";
+
+    // method name cannot use reserved keyword, e.g. return
+    if (isReservedWord(operationId)) {
+      throw new RuntimeException(operationId + " (reserved word) cannot be used as method name");
     }
-    return super.getTypeDeclaration(p);
+
+    return camelize(operationId, true);
   }
 
-  /**
-   * Optional - swagger type conversion.  This is used to map swagger types in a `Property` into 
-   * either language specific types via `typeMapping` or into complex models if there is not a mapping.
-   *
-   * @return a string value of the type or complex model for this property
-   * @see io.swagger.models.properties.Property
-   */
   @Override
-  public String getSwaggerType(Property p) {
-    String swaggerType = super.getSwaggerType(p);
-    String type = null;
-    if(typeMapping.containsKey(swaggerType)) {
-      type = typeMapping.get(swaggerType);
-      if(languageSpecificPrimitives.contains(type))
-        return toModelName(type);
+  public String toModelName(final String name) {
+    final String sanitizedName = sanitizeName(modelNamePrefix + this.stripPackageName(name) + modelNameSuffix);
+
+    // camelize the model name
+    // phone_number => PhoneNumber
+    final String camelizedName = camelize(sanitizedName);
+
+    // model name cannot use reserved keyword, e.g. return
+    if (isReservedWord(camelizedName)) {
+      final String modelName = "Model" + camelizedName;
+      LOGGER.warn(camelizedName + " (reserved word) cannot be used as model name. Renamed to " + modelName);
+      return modelName;
     }
-    else
-      type = swaggerType;
-    return toModelName(type);
+
+    // model name starts with number
+    if (name.matches("^\\d.*")) {
+      final String modelName = "Model" + camelizedName; // e.g. 200Response => Model200Response (after camelize)
+      LOGGER.warn(name + " (model name starts with number) cannot be used as model name. Renamed to " + modelName);
+      return modelName;
+    }
+
+    return camelizedName;
   }
+
+  @Override
+  public String toEnumName(CodegenProperty property) {
+    return formatIdentifier(stripPackageName(property.baseName), true);
+  }
+
 }
